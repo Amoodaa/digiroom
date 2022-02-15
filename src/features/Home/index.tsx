570716b6d94e85d0fd7f753a5924f41a109a5712
Imad Shatali <amoodaa@gmail.com>
2022-02-16 01:21:36 +0200

    feat: added toolbar

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,16 +1,35 @@
-import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
-import { NewRoomForm } from "./NewRoomForm";
+import Divider from "@mui/material/Divider";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import { FindCourses } from "./FindCoursesForm";
 import { FindCoursesResults } from "./FindCoursesResults";
+import { NewRoomForm } from "./NewRoomForm";
 
 export const Home = () => {
   return (
-    <Container maxWidth="xl" sx={{ p: 2 }}>
-      <NewRoomForm />
-      <Divider sx={{ my: 2 }} />
-      <FindCourses />
-      <FindCoursesResults />
-    </Container>
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h5"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              Digi Room
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidth="xl" sx={{ p: 2 }}>
+        <NewRoomForm />
+        <Divider sx={{ my: 2 }} />
+        <FindCourses />
+        <FindCoursesResults />
+      </Container>
+    </>
   );
 };
