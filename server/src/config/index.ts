c5c98ee1173160ca433387257fce85d352b4e8e5
Imad Shatali <amoodaa@gmail.com>
2022-05-22 02:41:25 +0300

    fix: db url

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,4 +2,4 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, YOUTUBE_API_KEY } = process.env;
+export const { NODE_ENV, PORT, DB_URL, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, YOUTUBE_API_KEY } = process.env;
--- server/src/databases/index.ts
+++ server/src/databases/index.ts
@@ -1,3 +1,3 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_URL } from '@config';
 
-export const dbConnection = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+export const dbConnection = DB_URL;
